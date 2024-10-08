a566b2072a6bc2fa13b087cc9340f41827e2f9b5
AngeloCrisci <angelocrisci1998@gmail.com>
2024-10-08 17:01:35 +0200

    Added axios callback (need id)

--- APP/main.js
+++ APP/main.js
@@ -7,9 +7,15 @@ const app = createApp({
     }
   },
   methods: {
-
+   
   },
   created(){
-
+    axios.get()
+        .then(function (response) {
+        console.log(response);
+        })
+        .catch(function (error) {
+        console.log(error);
+        })
   }
 }).mount('#app')
